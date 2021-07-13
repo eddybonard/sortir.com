74d59244b332c6c4008757984c37f1cc3dc9afc0
eddybONRD <eddyikzh@gmail.com>
2021-07-13 13:27:04 +0200

    creation des relations entre Entity (Participant, Campus, Sortie, Etat, Ville, Lieu)

--- src/Entity/Participant.php
+++ src/Entity/Participant.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ParticipantRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -32,10 +34,6 @@ class Participant
      */
     private $telephone;
 
-    /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $mail;
 
     /**
      * @ORM\Column(type="boolean")
@@ -46,6 +44,28 @@ class Participant
      * @ORM\Column(type="boolean")
      */
     private $actif;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Campus::class, inversedBy="participants")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $campus;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Sortie::class, mappedBy="organisateur")
+     */
+    private $organisateurSorties;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Sortie::class, inversedBy="participants")
+     */
+    private $participantSorties;
+
+    public function __construct()
+    {
+        $this->organisateurSorties = new ArrayCollection();
+        $this->participantSorties = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -88,17 +108,6 @@ class Participant
         return $this;
     }
 
-    public function getMail(): ?string
-    {
-        return $this->mail;
-    }
-
-    public function setMail(string $mail): self
-    {
-        $this->mail = $mail;
-
-        return $this;
-    }
 
     public function getAdministrateur(): ?bool
     {
@@ -120,6 +129,72 @@ class Participant
     public function setActif(bool $actif): self
     {
         $this->actif = $actif;
+
+        return $this;
+    }
+
+    public function getCampus(): ?Campus
+    {
+        return $this->campus;
+    }
+
+    public function setCampus(?Campus $campus): self
+    {
+        $this->campus = $campus;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Sortie[]
+     */
+    public function getOrganisateurSorties(): Collection
+    {
+        return $this->organisateurSorties;
+    }
+
+    public function addOrganisateurSorty(Sortie $organisateurSorty): self
+    {
+        if (!$this->organisateurSorties->contains($organisateurSorty)) {
+            $this->organisateurSorties[] = $organisateurSorty;
+            $organisateurSorty->setOrganisateur($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOrganisateurSorty(Sortie $organisateurSorty): self
+    {
+        if ($this->organisateurSorties->removeElement($organisateurSorty)) {
+            // set the owning side to null (unless already changed)
+            if ($organisateurSorty->getOrganisateur() === $this) {
+                $organisateurSorty->setOrganisateur(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Sortie[]
+     */
+    public function getParticipantSorties(): Collection
+    {
+        return $this->participantSorties;
+    }
+
+    public function addParticipantSorty(Sortie $participantSorty): self
+    {
+        if (!$this->participantSorties->contains($participantSorty)) {
+            $this->participantSorties[] = $participantSorty;
+        }
+
+        return $this;
+    }
+
+    public function removeParticipantSorty(Sortie $participantSorty): self
+    {
+        $this->participantSorties->removeElement($participantSorty);
 
         return $this;
     }
