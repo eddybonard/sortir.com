79516524a0bded7dbd26cd2daad1db198748f759
eddybONRD <eddyikzh@gmail.com>
2021-07-16 10:31:26 +0200

    ville 2.1 + admincontroller

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ville;
+use App\Repository\CampusRepository;
+use App\Repository\VilleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin/ville", name="admin_ville")
+     */
+    public function adminVille(VilleRepository  $villeRepository, Request $request, EntityManagerInterface $entityManager)
+    {
+
+        $recherche = $request->request->get('ville');
+        if ($recherche != null )
+        {
+            dump($recherche);
+            $villes = $villeRepository->villeTrieeParMot($recherche);
+
+            return $this->render('main/ville.html.twig' , [
+                'villes' => $villes,
+            ]);
+        }
+
+
+
+        $ville = new Ville();
+        $nom = $request->request->get('nom');
+        $codePOstal = $request->request->get('codePostal');
+
+        if($nom != null && $codePOstal != null)
+        {
+            $ville->setNom(strtoupper($nom));
+            $ville->setCodePostal($codePOstal);
+            $entityManager->persist($ville);
+            $entityManager->flush();
+
+            $villes = $villeRepository->findAll();
+
+            $this->addFlash('success','Votre ville a été ajoutée');
+            return $this->render('main/ville.html.twig' , [
+                'villes' => $villes,
+            ]);
+
+        }
+
+
+        $villes = $villeRepository->findAll();
+
+        return $this->render('main/ville.html.twig' , [
+            'villes' => $villes,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/campus", name="admin_campus")
+     */
+    public function adminCampus(CampusRepository  $campusRepository, Request $request, EntityManagerInterface $entityManager)
+    {
+        $recherche = $request->request->get('campus');
+        if ($recherche != null )
+        {
+            dump($recherche);
+            $campus = $campusRepository->campusTrieeParMot($recherche);
+
+            return $this->render('main/ville.html.twig' , [
+                'campus' => $campus,
+            ]);
+        }
+
+        $ville = new Ville();
+        $nom = $request->request->get('nom');
+        $codePOstal = $request->request->get('codePostal');
+
+        if($nom != null && $codePOstal != null)
+        {
+            $ville->setNom(strtoupper($nom));
+            $ville->setCodePostal($codePOstal);
+            $entityManager->persist($ville);
+            $entityManager->flush();
+
+            $villes = $villeRepository->findAll();
+
+            $this->addFlash('success','Votre ville a été ajoutée');
+            return $this->render('main/ville.html.twig' , [
+                'villes' => $villes,
+            ]);
+
+        }
+
+
+        $campus = $campusRepository->findAll();
+
+        return $this->render('main/campus.html.twig' , [
+            'campus' => $campus,
+        ]);
+    }
+}
